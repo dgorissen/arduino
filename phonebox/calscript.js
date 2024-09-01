d50cf1cfd5cbca36ea3c3517e8b17973684542f6
Dirk Gorissen <dgorissen@gmail.com>
2024-09-01 10:42:04 +0100

    Avoid lock alarming on the last day

--- phonebox/calscript.js
+++ phonebox/calscript.js
@@ -9,70 +9,62 @@ function doGet(e) {
   }
 
   var calendars_selected = [];
-
+  
   for (var ii = 0; ii < calendars.length; ii++) {
     if (calendars[ii].isSelected()) {
       calendars_selected.push(calendars[ii]);
       Logger.log(calendars[ii].getName());
     }
   }
-
+  
   Logger.log("Old: " + calendars.length + " New: " + calendars_selected.length);
 
-  const now = new Date();
+  // Get the events for today
+  var currentTime = new Date();
   var start = new Date(); start.setHours(0, 0, 0);  // start at midnight
-  const oneday = 24 * 3600000; // [msec]
-  const stop = new Date(start.getTime() + 1 * oneday); //get appointments for the next 14 days
+  const oneday = 24*3600000; // [msec]
+  const stop = new Date(start.getTime() + oneday); // the next 24 hours
 
   var events = mergeCalendarEvents(calendars_selected, start, stop); //pull start/stop time
-
+    
   var str = '';
+
   for (var ii = 0; ii < events.length; ii++) {
-
     var event = events[ii];
-    var myStatus = event.getMyStatus();
 
-    // Define valid entryStatus to populate array
-    switch (myStatus) {
-      case CalendarApp.GuestStatus.OWNER:
-      case CalendarApp.GuestStatus.YES:
-      case CalendarApp.GuestStatus.NO:
-      case CalendarApp.GuestStatus.INVITED:
-      case CalendarApp.GuestStatus.MAYBE:
-      default:
-        break;
-    }
+    // Check if the event is named "Kids", is an all-day event, and it's not past midday on the last day
+    if (event.getTitle().toLowerCase() === "kids" && event.isAllDayEvent()) {
+      var eventEndDate = event.getEndTime();
+      var midday = new Date(eventEndDate);
+      midday.setHours(12, 0, 0, 0); // set to 12:00 PM on the last day of the event
 
-    // Show just every entry regardless of GuestStatus to also get events from shared calendars where you haven't set up the appointment on your own
-    // str += event.getStartTime() + ';' +
-    // event.getTitle() +';' + 
-    // event.isAllDayEvent() + ';';
-
-    if (event.getTitle().toLowerCase() == "kids") {
-      Logger.log("returning true");
-      return ContentService.createTextOutput("true");
+      if (currentTime < midday) {
+        Logger.log("returning true");
+        return ContentService.createTextOutput("true");
+      }
     }
   }
+
   Logger.log("returning false");
   return ContentService.createTextOutput("false");
 }
 
 function mergeCalendarEvents(calendars, startTime, endTime) {
-  var params = { start: startTime, end: endTime, uniqueIds: [] };
+  var params = { start:startTime, end:endTime, uniqueIds:[] };
   return calendars.map(toUniqueEvents_, params)
-    .reduce(toSingleArray_)
-    .sort(byStart_);
+                  .reduce(toSingleArray_)
+                  .sort(byStart_);
 }
 
-function toUniqueEvents_(calendar) {
+function toUniqueEvents_ (calendar) {
   return calendar.getEvents(this.start, this.end)
-    .filter(onlyUniqueEvents_, this.uniqueIds);
+                 .filter(onlyUniqueEvents_, this.uniqueIds);
 }
 
 function onlyUniqueEvents_(event) {
   var eventId = event.getId();
   var uniqueEvent = this.indexOf(eventId) < 0;
-  if (uniqueEvent) this.push(eventId);
+  if(uniqueEvent) this.push(eventId);
   return uniqueEvent;
 }
 
