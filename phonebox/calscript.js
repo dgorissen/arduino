49c6051657ade66a1e3e1c80c2cf100cd619c3fd
Dirk Gorissen <dgorissen@gmail.com>
2025-08-02 22:41:17 +0100

    Cleanup calender script

--- phonebox/calscript.js
+++ phonebox/calscript.js
@@ -1,6 +1,5 @@
-/* Inspiration / source -  https://www.instructables.com/E-Ink-Family-Calendar-Using-ESP32/ */
-
 function doGet(e) {
+  // Return true if today is a phone locking day
   var calendars = CalendarApp.getCalendarsByName('Dirk Kids');
 
   if (calendars == undefined) {
@@ -8,16 +7,7 @@ function doGet(e) {
     return ContentService.createTextOutput("no access to calendar");
   }
 
-  var calendars_selected = [];
-  
-  for (var ii = 0; ii < calendars.length; ii++) {
-    if (calendars[ii].isSelected()) {
-      calendars_selected.push(calendars[ii]);
-      Logger.log(calendars[ii].getName());
-    }
-  }
-  
-  Logger.log("Old: " + calendars.length + " New: " + calendars_selected.length);
+  var kids_cal = calendars[0];
 
   // Get the events for today
   var currentTime = new Date();
@@ -25,51 +15,28 @@ function doGet(e) {
   const oneday = 24*3600000; // [msec]
   const stop = new Date(start.getTime() + oneday); // the next 24 hours
 
-  var events = mergeCalendarEvents(calendars_selected, start, stop); //pull start/stop time
-    
-  var str = '';
+  var kids_events = kids_cal.getEvents(start, stop)
+                 .filter((x) => {
+                    var r = x.isAllDayEvent() && x.getTitle().toLowerCase() === "kids";
+                    return r;
+                  });
 
-  for (var ii = 0; ii < events.length; ii++) {
-    var event = events[ii];
-
-    // Check if the event is named "Kids", is an all-day event, and it's not past midday on the last day
-    if (event.getTitle().toLowerCase() === "kids" && event.isAllDayEvent()) {
-      var eventEndDate = event.getEndTime();
-      var midday = new Date(eventEndDate);
-      midday.setHours(12, 0, 0, 0); // set to 12:00 PM on the last day of the event
-
-      if (currentTime < midday) {
-        Logger.log("returning true");
-        return ContentService.createTextOutput("true");
-      }
-    }
+  if (kids_events.length < 1) {
+    Logger.log("returning false");
+    return ContentService.createTextOutput("false");
   }
 
-  Logger.log("returning false");
-  return ContentService.createTextOutput("false");
-}
-
-function mergeCalendarEvents(calendars, startTime, endTime) {
-  var params = { start:startTime, end:endTime, uniqueIds:[] };
-  return calendars.map(toUniqueEvents_, params)
-                  .reduce(toSingleArray_)
-                  .sort(byStart_);
-}
-
-function toUniqueEvents_ (calendar) {
-  return calendar.getEvents(this.start, this.end)
-                 .filter(onlyUniqueEvents_, this.uniqueIds);
-}
-
-function onlyUniqueEvents_(event) {
-  var eventId = event.getId();
-  var uniqueEvent = this.indexOf(eventId) < 0;
-  if(uniqueEvent) this.push(eventId);
-  return uniqueEvent;
-}
-
-function toSingleArray_(a, b) { return a.concat(b) }
-
-function byStart_(a, b) {
-  return a.getStartTime().getTime() - b.getStartTime().getTime();
+  var event = kids_events[0];
+  var eventStartDate = event.getStartTime();
+  var eventEndDate = event.getEndTime(); //this resolves to the next day 00:00:00
+  var cutoff = new Date(eventEndDate);
+  cutoff.setHours(cutoff.getHours() - 12); //noon on the last day
+  
+  if (currentTime < cutoff) {
+    Logger.log("returning true");
+    return ContentService.createTextOutput("true");
+  } else {
+    Logger.log("returning false");
+    return ContentService.createTextOutput("false");
+  }
 }
